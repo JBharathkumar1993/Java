fe715b1f406fe49640cdeaa60aab9be7c2433608
Bharath <bharathtechnicalmail@gmail.com>
2020-06-12 14:24:33 +0530

    Updated Regex Example - 4

--- src/main/java/com/java/regularexpressions/Quantifier.java
+++ src/main/java/com/java/regularexpressions/Quantifier.java
@@ -16,6 +16,7 @@ public class Quantifier {
             System.out.println("Start Index of matched String : "+myMat1.start()+"; Matched String : "+myMat1.group());
         }
 
+        // Here last b's index is 8 but cursor executed till 9th position as we know cursor moves to index.length()+1 in matcher method which indicates 0 match
         Pattern myPat2 = Pattern.compile("a*");
         Matcher myMat2 = myPat2.matcher(target);
         System.out.println("\nAny number of times search");
@@ -24,6 +25,7 @@ public class Quantifier {
             System.out.println("Start Index of matched String : "+myMat2.start()+"; Matched String : "+myMat2.group());
         }
 
+        // Here last b's index is 8 but cursor executed till 9th position as we know cursor moves to index.length()+1 in matcher method which indicates 0 match
         Pattern myPat3 = Pattern.compile("a?");
         Matcher myMat3 = myPat3.matcher(target);
         System.out.println("\nAtmost 1 a means 1 a or 0 a's");

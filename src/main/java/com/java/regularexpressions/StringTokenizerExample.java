6e3d46e28fb72435b3680192b618e3b15874c130
Bharath <bharathtechnicalmail@gmail.com>
2020-06-12 15:15:05 +0530

    Updated Regex Example - 5

--- src/main/java/com/java/regularexpressions/StringTokenizerExample.java
+++ src/main/java/com/java/regularexpressions/StringTokenizerExample.java
@@ -6,9 +6,16 @@ public class StringTokenizerExample {
 
     public static void main(String[] args) {
 
-        StringTokenizer myToken = new StringTokenizer("This is Bharath");
-        while(myToken.hasMoreTokens()){
-            System.out.println(myToken.nextToken());
+        StringTokenizer myToken1 = new StringTokenizer("This is Bharath");
+        while(myToken1.hasMoreTokens()){
+            System.out.println(myToken1.nextToken());
+        }
+
+        System.out.println("--------------------");
+
+        StringTokenizer myToken2 = new StringTokenizer("www.facebook.com","\\.");
+        while(myToken2.hasMoreTokens()){
+            System.out.println(myToken2.nextToken());
         }
     }
 }
